9bab59d69e0001df57ced29fcac72ad9b531f6c7
andriiglukhyi <andrigluxii@gmail.com>
2018-02-13 11:19:00 -0800

    new

--- ex1/computeCostMulti.m
+++ ex1/computeCostMulti.m
@@ -13,8 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+% hypotesis = thrta*y
 
-
+% J= (1/(2*m))*sum(hypotesis - y)
 
 
 % =========================================================================
